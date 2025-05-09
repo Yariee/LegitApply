a394c90b61d07ffcd5e7ca1a2f6307c78c51f5e0
Yair <ballinasyair@gmail.com>
2025-05-08 20:37:58 -0500

    added request limits for linkedin, Selenium Setup, Linkedin Login Setup and Scraping Jobs

--- main.py
+++ main.py
@@ -20,3 +20,77 @@ SEARCH_URL = "https://www.linkedin.com/jobs/software-engineer-jobs-houston-tx/?c
 USERNAME = "your_email@example.com"
 PASSWORD = "your_password"
 
+# --- Request Limitations ----
+def load_request_log():
+    if os.path.exists(REQUEST_LOG):
+        with open(REQUEST_LOG, "r") as f:
+            return json.load(f)
+    return []
+
+def save_request_log(log):
+    with open(REQUEST_LOG, "w") as f:
+        json.dump(log, f)
+
+def can_make_request():
+    log = load_request_log()
+    one_week_ago = datetime.now() - timedelta(weeks = 1)
+    recent_requests = [t for t in log if datetime.fromisoformat(t) > one_week_ago]
+    return len(recent_requests)
+
+def record_request():
+    log = load_request_log()
+    log.append(datetime.now().isoformat())
+    save_request_log(log)
+
+
+# Selenium Setup
+def create_driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+    return webdriver.Chrome(options=options)
+
+
+# Linkedin Login Setup
+def login(driver):
+    print("Logging on into Linkedin..")
+    driver.get("https://www.linkedin.com/login")
+    time.sleep(2)
+
+    driver.find_element(By.ID, "username").send_keys(USERNAME)
+    driver.find_element(By.ID, "passoword").send_keys(PASSWORD)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    time.sleep(5)
+
+# Go to Job Searching Page
+def go_to_job_search(driver):
+    print(f"Navigating Job Search page: {SEARCH_URL}")
+    driver.get(SEARCH_URL)
+    time.sleep(5)
+
+# Scraping Jobs
+def scrape_jobs(driver):
+    print("Scraping Jobs..")
+    jobs = []
+    for _ in range(3):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(random.uniform(3, 5))
+
+    job_cards = driver.find_elements(By.CLASS_NAME, "job-card-container")
+    for card in job_cards:
+        try:
+            title = card.find_element(By.CLASS_NAME, "job-card-list_title").text
+            company = card.find_element(By.CLASS_NAME, "job-card-container_company_name").text
+            link = card.find_element(By.TAG_NAME, "a").get_attribute("href")
+            promoted = "promoted" in card.text.lower()
+
+            jobs.append({
+                "title": title,
+                "company": company,
+                "link": link,
+                "promote": promoted
+            })
+        except Exception as e:
+            print("Unexpected Exception of: ", e)
+
+    print(f"Scraped {len(jobs)} jobs")
+    return jobs
