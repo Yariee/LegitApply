fd21a9baf444b9cc8cc330746ba08c7efadcd4e7
Yair <ballinasyair@gmail.com>
2025-05-08 21:12:46 -0500

    added .env way to use login credentials for LinkedIn

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+from dotenv import load_dotenv
+
 
 # CONFIG
+load_dotenv()
 REQUEST_LOG= "request_log.json"
 JOBS_CSV = "data/linkedin_jobs.csv"
 MAX_WEEKLY_REQUESTS = 25    # 25 so we don't get banned lol
 SEARCH_URL = "https://www.linkedin.com/jobs/software-engineer-jobs-houston-tx/?currentJobId=4224572050"
-USERNAME = "your_email@example.com"
-PASSWORD = "your_password"
+USERNAME = os.getenv("LINKEDIN_USERNAME")
+PASSWORD = os.getenv("LINKEDIN_PASSWORD")
 
 # --- Request Limitations ----
 def load_request_log():
